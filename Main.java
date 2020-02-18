15a88bc146d4dd241be4253fb605c7f7061585eb
Canaleija <rocruzl@ipn.mx>
2020-02-18 09:54:50 -0600

    - clase persona y la creación de objetos con constructor definido

--- Main.java
+++ Main.java
@@ -1,7 +1,24 @@
+
+import entidades.Persona;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        
+        int x = 19;
+        char c= 34;   
+        System.out.println();
+        // ejecutar un metodo Constructor 
+        Persona beto = new Persona(23, 170, 67.4);
+        Persona martha = new Persona(0,0,0) ;
+        System.out.println();
+        beto.edad = 98;
+        System.out.println();
+
+       
+    
+
+      
     }
 
 }
---
+++ entidades/Persona.java
@@ -0,0 +1,18 @@
+package entidades;
+
+public class Persona{
+
+    public int edad;
+    int altura;
+    double peso;
+
+    public Persona(int e, int a, double p){
+        edad = e;
+        altura = a;
+        peso = p;
+
+    }
+
+    
+    
+}
