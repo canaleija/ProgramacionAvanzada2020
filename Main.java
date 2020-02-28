4392869a95bbd1a6e79573e4c63d47b48f5e02d6
Canaleija <rocruzl@ipn.mx>
2020-02-28 12:45:07 -0600

    - Encapsulamiento

--- Main.java
+++ Main.java
@@ -4,16 +4,18 @@ import entidades.Persona;
 public class Main {
 
     public static void main(String[] args) {
-        
-        int x = 19;
-        char c= 34;   
+       
         System.out.println();
         // ejecutar un metodo Constructor 
         Persona beto = new Persona(23, 170, 67.4);
-        Persona martha = new Persona(0,0,0) ;
+        Persona martha = new Persona();
+        martha.setAltura(67);
+        martha.setPeso(56.7);
+        martha.setEdad(17);
         System.out.println();
-        beto.edad = 98;
-        System.out.println();
+        beto.setEdad(56);
+        int x = beto.getAltura();
+        System.out.println(x);
 
        
     
--- entidades/Persona.java
+++ entidades/Persona.java
@@ -1,17 +1,51 @@
 package entidades;
 
+
+
 public class Persona{
 
-    public int edad;
-    int altura;
-    double peso;
+    private int edad;
+    private int altura;
+    private double peso;
+    
+    public Persona(){
+       edad = 0;
+       altura = 0;
+       peso = 0; 
+    }
 
     public Persona(int e, int a, double p){
         edad = e;
         altura = a;
         peso = p;
-
     }
+    
+    // metodos get y set de edad
+    public int getEdad(){
+        return edad;
+    }
+    public void setEdad(int edad){
+        this.edad = edad ;
+    }
+
+    // metodos get y set de edad
+    public int getAltura(){
+        return altura;
+    }
+    public void setAltura(int altura){
+        this.altura = altura ;
+    }
+    
+    // metodos get y set de edad
+    public double getPeso(){
+        return edad;
+    }
+    public void setPeso(double peso){
+        this.peso = peso ;
+    }
+   
+
+
 
     
     
